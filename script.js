b6e9c11a0829d52e02f2f92cadcbf1c781f079e3
jacky32 <patrki@atlas.cz>
2022-05-31 19:39:51 +0200

    Update password validation

--- script.js
+++ script.js
@@ -3,51 +3,33 @@ const passwordConfirmation = document.getElementById('password_confirmation');
 
 password.addEventListener('input', (event) => {
   if(event.target.value == passwordConfirmation.value) {
-    password.classList.remove('error');
-    passwordConfirmation.classList.remove('error');
+    removeErrorClass();
   } else {
-    password.classList.add('error');
-    passwordConfirmation.classList.add('error');
+    addErrorClass();
   }
 });
 passwordConfirmation.addEventListener('input', (event) => {
   if(password.value == event.target.value) {
-    password.classList.remove('error');
-    passwordConfirmation.classList.remove('error');
+    removeErrorClass();
   } else {
-    password.classList.add('error');
-    passwordConfirmation.classList.add('error');
+    addErrorClass();
   }
 });
 
+function validate() {
+  return document.getElementById('signup_form').reportValidity();
+}
 
+function removeErrorClass() {
+  password.classList.remove('error');
+  passwordConfirmation.classList.remove('error');
+  password.setCustomValidity("");
+  passwordConfirmation.setCustomValidity("");
+}
 
-function validatePasswords() {
-  if(password.value != passwordConfirmation.value) {
-    if(!password.classList.contains('error')) {
-      password.classList.add('error');
-      passwordConfirmation.classList.add('error');
-      password.addEventListener('change', (event) => {
-        if(event.target.value == passwordConfirmation.value) {
-          password.classList.remove('error');
-          passwordConfirmation.classList.remove('error');
-          return true;
-        }
-      });
-      passwordConfirmation.addEventListener('change', (event) => {
-        if(passwordConfirmation.value == event.target.value) {
-          password.classList.remove('error');
-          passwordConfirmation.classList.remove('error');
-          return true;
-        }
-      });
-      return false;
-    }
-  } else {
-    if(password.classList.contains('error')) {
-      password.classList.remove('error');
-      passwordConfirmation.classList.remove('error');
-      return true;
-    }
-  }
-};
+function addErrorClass() {
+  password.classList.add('error');
+  passwordConfirmation.classList.add('error');
+  password.setCustomValidity("Passwords don't match.");
+  passwordConfirmation.setCustomValidity("Passwords don't match.");
+}
